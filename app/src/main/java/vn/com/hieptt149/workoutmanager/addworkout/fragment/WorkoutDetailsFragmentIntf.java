ec01d9be76d8491cfef677f71e1fde88219716f1
hieptt149 <hieptt@gmail.com>
2018-04-04 09:08:59 +0700

    04042018-0900

---
+++ app/src/main/java/vn/com/hieptt149/workoutmanager/addworkout/fragment/WorkoutDetailsFragmentIntf.java
@@ -0,0 +1,11 @@
+package vn.com.hieptt149.workoutmanager.addworkout.fragment;
+
+import vn.com.hieptt149.workoutmanager.model.Exercise;
+
+/**
+ * Created by Administrator on 04/03/2018.
+ */
+
+public interface WorkoutDetailsFragmentIntf {
+    void onExerciseItemClick(Exercise selectedExercise);
+}
